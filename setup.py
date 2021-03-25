88de80e0bd2df8b61ab35ce362a4047469443640
textSolver34761 <pradonbenjamin.gmail.com>
2021-03-25 23:40:22 +0100

    updates

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="API_CSV",
+    name="APICSV",
     version="0.0.1",
     author="Benjamin Pradon",
     author_email="pradonbenjamin@gmail.com",
